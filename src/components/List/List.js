d679843969ec90a22ecd5a77beb309348a9a74c4
Kacper Koza <kacperkoza34@gmail.com>
2019-10-09 22:11:55 +0200

    used dataStore

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,27 +3,36 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
+
+
 
 
 
 class List extends React.Component {
 
+  state = {
+    columns: this.props.columns || [],
+  }
+
   static propTypes = {
   title: PropTypes.node.isRequired,
-  children: PropTypes.node.isRequired,
+  description: PropTypes.node,
+  columns: PropTypes.array,
   }
 
   static defaultProps = {
-  children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
   }
 
   render() {
-    console.log(this.props.title);
+    //console.log(this.props.title);
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} background={this.props.background} />
         <div className={styles.description}>
-          {this.props.children}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
           <Column text={'first'}/>
