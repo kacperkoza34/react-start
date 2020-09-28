aaca178fa2612e63d6a797b4763c91b406706965
Kacper Koza <kacperkoza34@gmail.com>
2020-09-28 18:26:57 +0200

    CHange base url

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,8 +6,8 @@ import Faq from '../Faq/Faq';
 import List from '../List/ListContainer';
 import SearchResult from '../SearchResult/SearchResultContainer';
 
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import {AnimatedSwitch} from 'react-router-transition';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 import styles from './App.scss';
 
 const App = () => (
@@ -19,14 +19,12 @@ const App = () => (
         atActive={{ opacity: 1 }}
         className={styles.switchWrapper}
       >
-        <Route exact path='/' component={Home} />
-        <Route exact path='/react-start/test' component={Info} />
-        <Route exact path='/react-start/faq' component={Faq} />
-        <Route exact path='/react-start/list/:id' component={List} />
-        <Route exact path='/react-start/search/:id' component={SearchResult} />
-        <Route exact path='/react-start/search/' component={Home} />
-
-
+        <Route exact path="/react-start/" component={Home} />
+        <Route exact path="/react-start/test" component={Info} />
+        <Route exact path="/react-start/faq" component={Faq} />
+        <Route exact path="/react-start/list/:id" component={List} />
+        <Route exact path="/react-start/search/:id" component={SearchResult} />
+        <Route exact path="/react-start/search/" component={Home} />
       </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import {NavLink, Link} from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import styles from './Header.scss';
 import Container from '../Container/Container';
-import {settings} from '../../data/dataStore';
+import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 import Search from '../Search/SearchContainer';
-
-
 
 const Header = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>
       <Container>
         <div className={styles.wrapper}>
-          <NavLink exact to='/' className={styles.logo}>{ReactHtmlParser(settings.header.icon)}</NavLink>
+          <NavLink exact to="/react-start/" className={styles.logo}>
+            {ReactHtmlParser(settings.header.icon)}
+          </NavLink>
           <Search />
-          <NavLink exact to='/'>Home</NavLink>
-          <NavLink exact to='/react-start/test'>Info</NavLink>
-          <NavLink exact to='/react-start/faq'>FAQ</NavLink>
+          <NavLink exact to="/react-start/">
+            Home
+          </NavLink>
+          <NavLink exact to="/react-start/test">
+            Info
+          </NavLink>
+          <NavLink exact to="/react-start/faq">
+            FAQ
+          </NavLink>
         </div>
       </Container>
     </h2>
